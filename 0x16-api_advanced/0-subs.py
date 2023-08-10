52abaa3ae6fc955278e016e7cef2261b129ba170
CyrusNchege <cyruschegecc@gmail.com>
2023-08-10 20:07:06 +0300

    [Chore]: Add python script that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''queries the Reddit API and returns the number of subscribers'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''returns the number of subscribers'''
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python3'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
