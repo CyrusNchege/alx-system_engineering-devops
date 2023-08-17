056f802b32a2cf8d547351cf45e7f0e1ae6ad446
CyrusNchege <cyruschegecc@gmail.com>
2023-08-17 09:36:47 -0700

    Refactor: improving puppet file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,40 +1,14 @@
-# This is a Puppet manifest to configure Nginx settings
+# Increases the amount of traffic an Nginx server can handle
 
-# Ensure Nginx package is installed
-package { 'nginx':
-  ensure => installed,
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
 }
 
-# Stop and disable any existing Nginx service
-service { 'nginx':
-  ensure => stopped,
-  enable => false,
+#restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
-# Configure Nginx virtual host
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('your_module/nginx_config.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable the Nginx virtual host
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-  notify => Service['nginx'],
-}
-
-# Restart Nginx service when configuration changes
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['/etc/nginx/sites-enabled/default'],
-}
-
-# Example nginx_config.erb template
-# Replace this with your actual Nginx configuration template
-# e.g., setting worker processes, adjusting connection settings, etc.
-
